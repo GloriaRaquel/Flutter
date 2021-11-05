68d86b81fd0d4375a20f424f377ea2f0e466a7f9
GloriaRaquel <93741341+GloriaRaquel@users.noreply.github.com>
2021-11-04 20:49:33 -0600

    Diferido3
    Diferido3 de flutter

---
+++ flutter_examen/lib/main.dart
@@ -0,0 +1,186 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Parcial',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _direccionController = TextEditingController();
+   final TextEditingController _descripciomController = TextEditingController();
+
+  CollectionReference _productss =
+      FirebaseFirestore.instance.collection('depto');
+
+  // This function is triggered when the floatting button or one of the edit buttons is pressed
+  // Adding a product if no documentSnapshot is passed
+  // If documentSnapshot != null then update an existing product
+  Future<void> _createOrUpdate([DocumentSnapshot? documentSnapshot]) async {
+    String action = 'create';
+    if (documentSnapshot != null) {
+      action = 'update';
+      _nameController.text = documentSnapshot['nombre'];
+      _descripciomController.text=documentSnapshot['descripcion'];
+      _direccionController.text = documentSnapshot['direcion'];
+    }
+
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _nameController,
+                  decoration: InputDecoration(labelText: 'Nombre'),
+                ),
+                 TextField(
+                  controller: _direccionController,
+                  decoration: InputDecoration(labelText: 'Direccion'),
+                ),
+                 TextField(
+                  controller: _descripciomController,
+                  decoration: InputDecoration(labelText: 'Descripcion'),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: Text(action == 'create' ? 'Create' : 'Update'),
+                  onPressed: () async {
+                    final String? name = _nameController.text;
+                    final String? desc=_descripciomController.text;
+                    final String? direc=_direccionController.text;
+                    if (name != null && desc != null && direc != null) {
+                      if (action == 'create') {
+                        // Persist a new product to Firestore
+                        await _productss.add({"nombre": name,"descripcion":desc, "direcion": direc});
+                      }
+
+                      if (action == 'update') {
+                        // Update the product
+                        await _productss
+                            .doc(documentSnapshot!.id)
+                            .update({"nombre": name,"descripcion":desc, "direcion": direc});
+                      }
+
+                      // Clear the text fields
+                      _nameController.text = '';
+                      _direccionController.text="";
+                      _descripciomController.text="";
+
+                      // Hide the bottom sheet
+                      Navigator.of(context).pop();
+                    }
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  // Deleteing a product by id
+  Future<void> _deleteProduct(String productId) async {
+    await _productss.doc(productId).delete();
+
+    // Show a snackbar
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('You have successfully deleted a product')));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Parcial'),
+        leading: IconButton(
+          icon: Icon(Icons.people_outline),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+             
+
+            },),);
+          },
+        ),
+      ),
+      // Using StreamBuilder to display all products from Firestore in real-time
+      body: StreamBuilder(
+        stream: _productss.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.hasData) {
+            return ListView.builder(
+              itemCount: streamSnapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                final DocumentSnapshot documentSnapshot =
+                    streamSnapshot.data!.docs[index];
+                return Card(
+                  margin: EdgeInsets.all(10),
+                  child: ListTile(
+                    title: Text(documentSnapshot['nombre']),
+                    subtitle: Text(documentSnapshot['descripcion']),
+                    trailing: SizedBox(
+                      width: 100,
+                      child: Row(
+                        children: [
+                          // Press this button to edit a single product
+                          IconButton(
+                              icon: Icon(Icons.edit),
+                              onPressed: () =>
+                                  _createOrUpdate(documentSnapshot)),
+                          // This icon button is used to delete a single product
+                          IconButton(
+                              icon: Icon(Icons.delete),
+                              onPressed: () =>
+                                  _deleteProduct(documentSnapshot.id)),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+      // Add new product
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _createOrUpdate(),
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
